fea0179060dd5507e9773ace16f3caa2d2ce413b
himanshuparmar21 <himanshu.parmar21204@gmail.com>
2024-06-20 18:00:23 +0530

    20-06-2024

--- src/CodeForces/YogurtSale.java
+++ src/CodeForces/YogurtSale.java
@@ -1,2 +1,21 @@
-package CodeForces;public class YogurtSale {
+package CodeForces;
+
+import java.util.Scanner;
+
+public class YogurtSale {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int costWithPromotion = (n / 2) * b + (n % 2) * a;
+            int costWithoutPromotion = n * a;
+
+            System.out.println(Math.min(costWithPromotion, costWithoutPromotion));
+        }
+    }
 }
--- src/Demo.java
+++ src/Demo.java
@@ -2,33 +2,33 @@ import java.util.*;
 
 class Demo {
     public static void main(String[] args) {
-//        System.out.println(maxProfitAssignment(new int[]{2,4,6,8,10},new int[]{10,20,30,40,50},new int[]{4,5,6,7}));
+        System.out.println(longestConsecutive(new int[]{100,4,200,1,3,2}));
     }
-    static int rectanglesInCircle(int radius) {
-        int r = 0;
+        static int longestConsecutive(int[] nums) {
+            List<Integer> set = new ArrayList<>();
 
-        int diameter = 2 * radius;
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
 
-        int ds = diameter *
-                diameter;
-
-        for (int a = 1;
-             a < 2 * radius; a++)
-        {
-            for (int b = 1;
-                 b < 2 * radius; b++)
-            {
-
-                int diagonalLengthSquare = (a * a +
-                        b * b);
-
-                if (diagonalLengthSquare <= ds)
-                {
-                    r++;
-                }
+            for(int num : nums){
+                set.add(num);
+                min = Math.min(min,num);
+                max = Math.max(max,num);
             }
-        }
 
-        return r;
+            Collections.sort(set);
+            int count = 0;
+            int ans = 0;
+            while(min<=max){
+                if(set.contains(min)){
+                    count++;
+                }else{
+                    count = 0;
+                }
+                ans = Math.max(ans,count);
+                min++;
+            }
+            return count;
+
     }
 }
