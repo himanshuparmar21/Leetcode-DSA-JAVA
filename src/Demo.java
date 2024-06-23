2fb84ec83c53c5b154e431ad3a65afcfbbe6e1ee
himanshuparmar21 <himanshu.parmar21204@gmail.com>
2024-06-23 12:32:24 +0530

    23-06-2024

--- src/Demo.java
+++ src/Demo.java
@@ -2,24 +2,27 @@ import java.math.BigInteger;
 import java.util.*;
 
 class Demo {
-    public static void main(String[] args) {
-        System.out.println(numberOfSubarrays(new int[]{2,2,2,1,2,2,1,2,2,2},2));
-    }
-    static public int numberOfSubarrays(int[] nums, int k) {
-        int ans = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            int count = 0;
-            for (int j = i; j < nums.length; j++) {
-                if(nums[i] % 2 != 0){
-                    count++;
-                }
-                if(count==k){
-                    ans++;
-                    count=0;
-                }
-            }
-        }
-        return ans;
-    }
+//    public static void main(String[] args) {
+//        System.out.println(bracketNumbers("(aa(bdc))p(dee)"));
+//    }
+//    static ArrayList<Integer> bracketNumbers(String str) {
+//        Stack<Character> st = new Stack<>();
+//        ArrayList<Integer> list = new ArrayList<>();
+//        int temp = 0;
+//        int count = 0;
+//
+//        for(char ch : str.toCharArray()){
+//            if(ch=='('){
+//                count++;
+//                temp++;
+//                st.add(ch);
+//                list.add(count);
+//            }
+//            if(ch==')'){
+//                list.add(temp);
+//                temp--;
+//
+//            }
+//        }
+//    }
 }
