5228170cc2df414cf59ab8d13ca62742c26ae6a4
himanshuparmar21 <himanshu.parmar21204@gmail.com>
2024-06-24 10:37:09 +0530

    24-06-2024

--- src/Demo.java
+++ src/Demo.java
@@ -2,27 +2,24 @@ import java.math.BigInteger;
 import java.util.*;
 
 class Demo {
-//    public static void main(String[] args) {
-//        System.out.println(bracketNumbers("(aa(bdc))p(dee)"));
-//    }
-//    static ArrayList<Integer> bracketNumbers(String str) {
-//        Stack<Character> st = new Stack<>();
-//        ArrayList<Integer> list = new ArrayList<>();
-//        int temp = 0;
-//        int count = 0;
-//
-//        for(char ch : str.toCharArray()){
-//            if(ch=='('){
-//                count++;
-//                temp++;
-//                st.add(ch);
-//                list.add(count);
-//            }
-//            if(ch==')'){
-//                list.add(temp);
-//                temp--;
-//
-//            }
-//        }
-//    }
+    public static void main(String[] args) {
+        System.out.println(findMaxAverage(new int[]{1,12,-5,-6,50,3},4));
+    }
+    static public double findMaxAverage(int[] nums, int k) {
+        List<Double> set = new ArrayList<>();
+
+        double sum = 0;
+        int start = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (i >= k-1){
+                set.add((double) (sum/k));
+                sum -= nums[start];
+                start++;
+            }
+        }
+        set.sort(Collections.reverseOrder());
+        return set.getFirst();
+    }
 }
