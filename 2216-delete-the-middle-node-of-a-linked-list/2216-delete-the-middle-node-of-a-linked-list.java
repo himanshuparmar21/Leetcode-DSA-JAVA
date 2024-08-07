e275e263b4af87a02082aacbc5444eeb4ac0841f
Himanshu Parmar <113753642+himanshuparmar21@users.noreply.github.com>
2024-08-07 08:31:44 +0530

    Time: 3 ms (99.7%), Space: 64.7 MB (23.02%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
@@ -1,40 +1,32 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        // if (head == null || head.next == null) {
-        //     return null;
-        // }
-        // ListNode slow = head;
-        // ListNode fast = head;
-        // ListNode prev = null;
-        // while (fast != null && fast.next != null) {
-        //     prev = slow;
-        //     slow = slow.next;
-        //     fast = fast.next.next;
-        // }
-        // prev.next = slow.next;
-        // return head;
-        ListNode fast=head;
         ListNode slow=head;
-        if(head==null || head.next==null)return null;
-        if(head.next.next==null){
+        ListNode fast=head;
+
+        if(head==null || head.next==null) return null;
+        if(head.next.next == null){
             head.next=null;
-            return head;}
-        while(fast!=null && fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
+            return head;
         }
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
         slow.val=slow.next.val;
         slow.next=slow.next.next;
         return head;
+        
     }
 }
