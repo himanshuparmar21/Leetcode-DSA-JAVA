2f2692b5bbfeea44add1f0c044e451627e6064a6
Himanshu Parmar <113753642+himanshuparmar21@users.noreply.github.com>
2024-08-07 21:22:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (61.66%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,19 +9,29 @@
  * }
  */
 class Solution {
+    // public ListNode reverseList(ListNode head) {
+    //     ArrayList<Integer> list = new ArrayList<>();
+    //     ListNode temp = head;
+    //     while(temp != null){
+    //         list.add(temp.val);
+    //         temp = temp.next;
+    //     }
+    //     ListNode curr = head;
+    //     Collections.reverse(list);
+    //     for(int num : list){
+    //         curr.val = num;
+    //         curr = curr.next;
+    //     }
+    //     return head;
+    // }
     public ListNode reverseList(ListNode head) {
-        ArrayList<Integer> list = new ArrayList<>();
-        ListNode temp = head;
-        while(temp != null){
-            list.add(temp.val);
-            temp = temp.next;
-        }
-        ListNode curr = head;
-        Collections.reverse(list);
-        for(int num : list){
-            curr.val = num;
-            curr = curr.next;
-        }
-        return head;
+        return rev(head, null);
+    }
+
+    public ListNode rev(ListNode node, ListNode pre) {
+        if (node == null) return pre;
+        ListNode temp = node.next;
+        node.next = pre;
+        return rev(temp, node);
     }
 }
