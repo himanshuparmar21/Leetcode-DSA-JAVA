06b92021cb78bff1c3fd5d798bdbed54964e5b57
Himanshu Parmar <113753642+himanshuparmar21@users.noreply.github.com>
2024-08-23 21:41:08 +0530

    Time: 10 ms (36.66%), Space: 45.4 MB (12.65%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
@@ -10,31 +10,30 @@
  */
 class Solution {
     public ListNode insertGreatestCommonDivisors(ListNode head) {
-        ListNode p1 = head;
-        ListNode p2 = head.next;
+        ListNode temp = head,prev = head;
 
-        while(p2!=null){
-            int gcd = gcd(p1.val,p2.val);
-            ListNode newNode = new ListNode(gcd); 
-            p1.next=newNode;
-            newNode.next=p2;
-            p1=p2;
-            p2=p2.next;
+        while(temp.next !=null ){
+            int val = gcd(temp.val,temp.next.val);
+            ListNode node = new ListNode(val);
+            temp = temp.next;
+            prev.next = node;
+            node.next = temp;
+            prev = temp;
         }
         return head;
     }
-
-    static int gcd(int a, int b)
+    int gcd(int a, int b)
     {
-        int i;
-        if (a < b)
-            i = a;
-        else
-            i = b;
-        for (i = i; i > 1; i--) {
-            if (a % i == 0 && b % i == 0)
-                return i;
-        }
-        return 1;
+        if (a == 0)
+            return b;
+        if (b == 0)
+            return a;
+
+        if (a == b)
+            return a;
+
+        if (a > b)
+            return gcd(a - b, b);
+        return gcd(a, b - a);
     }
 }
