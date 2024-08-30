f2ffd8ffc2173442bca923a8065f40b9d141b58d
Himanshu Parmar <113753642+himanshuparmar21@users.noreply.github.com>
2024-08-30 19:20:20 +0530

    Time: 927 ms (9.08%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,2 +1,8 @@
 SELECT [name] AS Employee FROM Employee e1
 where salary > (select salary from Employee where id=e1.managerId)
+
+-- SELECT name AS Employee
+-- FROM Employee e1
+-- WHERE managerId IS NOT NULL
+-- AND managerId IN (SELECT id FROM Employee)
+-- and salary > (select salary from employee e2 where e1.managerId = e2.id);
