34d3c8e85d59e15e969fae718bdf86ffd9edbfe8
Himanshu Parmar <113753642+himanshuparmar21@users.noreply.github.com>
2024-09-24 22:20:18 +0530

    Time: 3 ms (71.42%), Space: 56.3 MB (83.06%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,22 +1,23 @@
 class Solution {
     public int longestSubarray(int[] nums) {
         int ans = 0;
-        //This is Brute Force Aprroch Generate All SubArray still at most k (means zero<=k) and Count Subaaray Length
-        // This take O(n) Time Complecity
-        for(int i=0;i<nums.length;i++){
-            int zero = 0;
-            for(int j=i;j<nums.length;j++){
-                if(nums[j]==0){
-                    zero++;
-                }
-                if(zero<=1){
-                    ans = Math.max(ans,(j-i));
-                }else{
-                    break;
-                }
+        int l = 0;
+        int r = 0;
+        int zero = 0;
+        while(r < nums.length){
+            if(nums[r] == 0){
+                zero++;
             }
+            if(zero<=1){
+                ans = Math.max(ans,(r-l));
+            }else{
+                if(nums[l]==0){
+                    zero--;
+                }
+                l++;
+            }
+            r++;
         }
-        if(ans == nums.length) return ans-1;
         return ans;
     }
 }
