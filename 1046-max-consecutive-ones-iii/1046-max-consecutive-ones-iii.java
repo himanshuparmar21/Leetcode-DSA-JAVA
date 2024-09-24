aa7f95ec3232d98d86bcf5eef2d0844e0317e020
Himanshu Parmar <113753642+himanshuparmar21@users.noreply.github.com>
2024-09-24 21:55:36 +0530

    Time: 3 ms (86.53%), Space: 47.3 MB (18.11%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,20 +1,19 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int ans = 0;
-        //This is Brute Force Aprroch Generate All SubArray still at most k (means zero<=k) and Count Subaaray Length
-        // This take O(n) Time Complecity
-        for(int i=0;i<nums.length;i++){
-            int zero = 0;
-            for(int j=i;j<nums.length;j++){
-                if(nums[j]==0){
-                    zero++;
-                }
-                if(zero<=k){
-                    ans = Math.max(ans,(j-i+1));
-                }else{
-                    break;
-                }
+        int l = 0;
+        int r = 0;
+        int zero = 0;
+        while(r < nums.length){
+            if(nums[r] == 0){
+                zero++;
             }
+            while(zero>k){
+                if(nums[l]==0) zero--;
+                l++;
+            }
+            ans = Math.max(ans,(r-l+1));
+            r++;
         }
         return ans;
     }
