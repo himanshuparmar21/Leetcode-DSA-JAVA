0741d487eee4597aa027560d1cc46a021e5edcc4
Himanshu Parmar <113753642+himanshuparmar21@users.noreply.github.com>
2024-09-25 22:51:31 +0530

    Time: 12 ms (70.97%), Space: 44.2 MB (91.76%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -3,7 +3,8 @@ class Solution {
         int[] temp = {-1,-1,-1};
 
         int count = 0;
-
+        // In This Problem First Find Three(a,b,c) index and Find Min ind and take Count of min+1;
+        // Like bbacb Here "acb" is minimum valid String and min ind is 2 then bbacb,bacb also valid String Then Total 3 Valid String(2+1); 
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
             temp[ch-'a'] = i;
