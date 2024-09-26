789fc58eaf58c0774f5d2d88e24881367169ee37
Himanshu Parmar <113753642+himanshuparmar21@users.noreply.github.com>
2024-09-26 11:03:50 +0530

    Time: 39 ms (12.2%), Space: 44.6 MB (18.01%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,22 +1,29 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int ans = 0;
-        // This is Brute Force Solution
-        // Find All SubString and Check How many max. change need
-        // And Then Check if Change nedd <= k then Take Max length of SubString
+        int start = 0;
+        int max = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+
         for (int i = 0; i < s.length(); i++) {
-            int[] temp = new int[26];
-            int max = 0;
-            for (int j = i; j < s.length(); j++) {
-                char ch = s.charAt(j);
-                temp[ch - 'A']++;
-                max = Math.max(max,temp[ch - 'A']);
-                int change = (j-i+1) - max;
-                if(change <= k){
-                    ans = Math.max(ans,(j-i+1));
-                }else{
-                    break;
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            max = Math.max(max,map.get(ch));
+            int change = (i-start+1) - max;
+            while (change > k){
+                ch = s.charAt(start);
+                map.put(ch,map.getOrDefault(ch,0)-1);
+                for (char key : map.keySet()){
+                    max = Math.max(max,map.get(key));
                 }
+                start++;
+                change = (i-start+1) - max;
+                if(map.get(ch)==0){
+                    map.remove(ch);
+                }
+            }
+            if(change <= k){
+                ans = Math.max(ans, (i - start + 1));
             }
         }
         return ans;
