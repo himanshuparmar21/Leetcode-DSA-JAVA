64d7b8bd0c4dfe1cca10ba231c79bf7613a2b46a
Himanshu Parmar <113753642+himanshuparmar21@users.noreply.github.com>
2024-09-30 18:29:22 +0530

    Time: 4 ms (47.81%), Space: 76.9 MB (66.25%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,19 +1,33 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int k = 0;
-        int i = 0;
-        int j = 0;
-        int n = nums.length;
+        // int k = 0;
+        // int i = 0;
+        // int j = 0;
+        // int n = nums.length;
+        // int[] ans = new int[n];
+        // while (k<n){
+        //     while(nums[i] < 0){
+        //         i++;
+        //     }
+        //     ans[k++] = nums[i++];
+        //     while(nums[j] > 0){
+        //         j++;
+        //     }
+        //     ans[k++] = nums[j++];
+        // }
+        // return ans;
+        int n =nums.length;
         int[] ans = new int[n];
-        while (k<n){
-            while(nums[i] < 0){
-                i++;
+        int pos=0;int neg=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                ans[pos]=nums[i];
+                pos+=2;
+            }else{
+                 ans[neg]=nums[i];
+                neg+=2;
+
             }
-            ans[k++] = nums[i++];
-            while(nums[j] > 0){
-                j++;
-            }
-            ans[k++] = nums[j++];
         }
         return ans;
     }
