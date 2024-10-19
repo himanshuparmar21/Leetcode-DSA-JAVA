bd1c6f4b874d72e5d36a3fe885ff86222ef5692e
Himanshu Parmar <113753642+himanshuparmar21@users.noreply.github.com>
2024-10-19 21:57:32 +0530

    Time: 11 ms (98.63%), Space: 47.3 MB (36.64%) - LeetHub

--- 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/2293-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
+++ 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/2293-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
@@ -1,22 +1,34 @@
 class Solution {
     public int minSteps(String s, String t) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        // HashMap<Character, Integer> map = new HashMap<>();
         
-        for (char ch : s.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-        for (char ch : t.toCharArray()) {
-            if (map.containsKey(ch)) {
-                map.put(ch, map.get(ch) - 1);
-            } else {
-                map.put(ch, map.getOrDefault(ch, 0) - 1);
-            }
-        }
-        int count = 0;
-        for (int freq : map.values()) {
-            count += Math.abs(freq);
-        }
+        // for (char ch : s.toCharArray()) {
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // }
+        // for (char ch : t.toCharArray()) {
+        //     if (map.containsKey(ch)) {
+        //         map.put(ch, map.get(ch) - 1);
+        //     } else {
+        //         map.put(ch, map.getOrDefault(ch, 0) - 1);
+        //     }
+        // }
+        // int count = 0;
+        // for (int freq : map.values()) {
+        //     count += Math.abs(freq);
+        // }
         
+        // return count;
+        int[] temp=new int[26];
+        for(char ch:s.toCharArray()){
+            temp[ch-'a']++;
+        }
+        for(char ch:t.toCharArray()){
+            temp[ch-'a']--;
+        }
+        int count=0;
+        for(int i:temp){
+            count+=Math.abs(i);
+        }
         return count;
     }
 }
