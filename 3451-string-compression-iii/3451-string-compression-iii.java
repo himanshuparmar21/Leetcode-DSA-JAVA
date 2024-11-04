d4f0e4bcd9d6f30acc8320deb196e658fd9dcd47
Himanshu Parmar <113753642+himanshuparmar21@users.noreply.github.com>
2024-11-04 20:40:38 +0530

    Time: 16 ms (94.92%), Space: 45.5 MB (91.88%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.java
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -1,5 +1,18 @@
 class Solution {
     public String compressedString(String word) {
-        
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while(i < word.length()){
+            int count=1;
+            char ch = word.charAt(i);
+            while(i < word.length()-1 && ch==word.charAt(i+1) && count!=9 ){
+                i++;
+                count++;
+            }
+            sb.append(count);
+            sb.append(ch);
+            i++;
+        }
+        return sb.toString();
     }
 }
